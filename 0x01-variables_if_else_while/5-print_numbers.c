b6489b8834c656d6830146dc41d25381d41a1a7b
Leticiamkabu <kabuleticia36@gmail.com>
2022-04-09 15:55:58 -0700

    printing numbers less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int counter;
+
+	for (counter = 0; counter < 10; counter++)
+		printf("%d", counter);
+
+	return (0);
+}

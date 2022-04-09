eed330393f20d2c0366eedcd2086fe921f794604
Leticiamkabu <kabuleticia36@gmail.com>
2022-04-09 07:10:25 -0700

    Print alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int letter;
+
+	for (letter = 97; letter <= 122; letter++)
+		putchar(letter);
+
+	return (0);
+}

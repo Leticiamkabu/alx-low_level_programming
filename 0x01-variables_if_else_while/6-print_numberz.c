9db6c3ed55d1d324c2d81220aa30cf44deee658d
Leticiamkabu <kabuleticia36@gmail.com>
2022-04-10 13:27:16 -0700

    print all single digit numbers of base 16

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+*/
+
+int main(void)
+{
+	int counter;
+
+	for (counter = 48; counter > 58; counter++)
+		putchar(counter);
+
+
+
+	return (0);
+}

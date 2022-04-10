ad3117b4678fcced8bb1ab9cb71122a5ec37caf1
Leticiamkabu <kabuleticia36@gmail.com>
2022-04-10 14:11:01 -0700

    print all the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int counter;
+
+	for (counter = 48; counter < 58; counter++)
+		putchar(counter);
+
+	putchar(counter);
+
+
+	return (0);
+}

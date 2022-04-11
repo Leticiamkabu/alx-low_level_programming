4d4264d1856ac78e94c98160aaedd3659830c411
Leticiamkabu <kabuleticia36@gmail.com>
2022-04-10 22:00:39 -0700

    display numbers and lower alphabets

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		putchar(44);
+		putchar(32);
+	}
+	
+
+	putchar(36);
+
+
+	return (0);
+}

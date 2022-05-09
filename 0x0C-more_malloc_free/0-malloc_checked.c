1fb4f2c21a2d18f67dc2d4043b770b9da9bcad43
Leticiamkabu <kabuleticia36@gmail.com>
2022-05-09 06:40:27 -0700

    Afunction that allocates memory for array using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,12 +7,12 @@
  *    * @b: the number to allocate memory for
  *     *
  *      * Return: pointer to the allocated memory
- */
+ *       */
 
 void *malloc_checked(unsigned int b)
 {
 	unsigned int *i;
-
+	
 	i = malloc(b);
 	if (i == NULL)
 	{
